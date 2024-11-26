a5acbf42cde1b5f3999704a8fef9f8cd0086264c
htilssu <tolashuu@gmail.com>
2024-11-26 23:58:11 +0700

    update cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -28,10 +28,11 @@ const isAllowedOrigin = true;
 export async function middleware(request: NextRequest) {
     const response = NextResponse.next();
 
-    response.headers.set('Access-Control-Allow-Origin', "*")
-    response.headers.set('Access-Control-Allow-Methods', "*")
+    response.headers.set('Access-Control-Allow-Origin', '*');
+    response.headers.set('Access-Control-Allow-Methods', '*');
+    response.headers.set('Access-Control-Allow-Headers', '*');
 
-    return response
+    return response;
 }
 
 export const config = {
