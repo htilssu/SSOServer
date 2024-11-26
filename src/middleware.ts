a6194e2c6f26f0548faacafae490451082ac5b84
htilssu <tolashuu@gmail.com>
2024-11-26 23:17:22 +0700

    add cors

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,18 +16,31 @@ import type {NextRequest} from 'next/server'
 import {NextResponse} from 'next/server'
 
 
-/*const corsOptions = {
+const allowedOrigins = ['https://wowo.htilssu.id.vn',]
+
+const corsOptions = {
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-}*/
+}
 
+const isAllowedOrigin = true;
 
 export async function middleware(request: NextRequest) {
+    const isPreflight = request.method === 'OPTIONS'
+
+    if (isPreflight) {
+        const preflightHeaders = {
+            ...(isAllowedOrigin && {'Access-Control-Allow-Origin': origin}),
+            ...corsOptions,
+        }
+        return NextResponse.json({}, {headers: preflightHeaders})
+    }
+
     return NextResponse.next()
 }
 
 export const config = {
     matcher: [
-        '/((?!.*cron|.*test.*|404|.*sso.*|.*password|.*sign-in.*|.*sign-up.*|_next|_next/image|favicon.ico|sitemap.xml|robots.txt|public).*)',
+        "/([.]+)"
     ]
 }
